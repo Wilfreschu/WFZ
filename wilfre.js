ba7bfa0d0511cdd0193fd6a1354bffa79bcf3a2e
Wilfreschu <Wschuchner@udesa.edu.ar>
2024-10-31 17:22:16 -0300

    mis archivos nuevos

---
+++ wilfre.js
@@ -0,0 +1,25 @@
+let firstName = "Wilfredo";
+console.log(firstName)
+let lastName = "Schuchner";
+console.log(lastName)
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName)
+let edad = 18;
+console.log(edad)
+let movies = ["robo del siglo", "spiderman", "cars2" , "cars1"];
+movies.pop()
+movies.push("cars 4", "up")
+console.log(movies)
+let learningJs = false;
+console.log(learningJs) 
+let carreer = "Administracion de empresas";
+console.log(carreer) 
+let univesity = "Udesa";
+console.log(univesity) 
+let personalinfo = ""
+learningJs = true;
+console.log(learningJs)
+carreer = "Negocios";
+console.log(carreer)
+univesity = "uade";
+console.log(univesity)
